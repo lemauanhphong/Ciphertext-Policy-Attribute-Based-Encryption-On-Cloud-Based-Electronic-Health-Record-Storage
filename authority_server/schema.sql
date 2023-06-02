40bf0075b995943ac46f03f58a60fb83947240a7
woanmeo11 <woanmeo11@gmail.com>
2023-06-02 10:58:56 +0700

    change role

--- authority_server/schema.sql
+++ authority_server/schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE
     id INT NOT NULL AUTO_INCREMENT,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    roles VARCHAR(255) NOT NULL,
+    role VARCHAR(255) NOT NULL,
     UNIQUE (username),
     PRIMARY KEY (id)
   );
