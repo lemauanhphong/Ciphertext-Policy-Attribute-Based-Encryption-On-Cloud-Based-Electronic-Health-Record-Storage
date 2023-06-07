1b878d8bf93f84a335cd16b9cbc74332c9dae167
lemauanhphong <lemauanhphong@gmail.com>
2023-06-07 22:29:35 +0700

    Fix AS app.py

--- authority_server/app.py
+++ authority_server/app.py
@@ -1,5 +1,5 @@
 from config import PASSWORD_HMAC_KEY, COOKIE_KEY
-from flask import Flask, request, Response, session
+from flask import Flask, request, session
 from flask_session import Session
 import werkzeug
 import hmac
@@ -8,7 +8,7 @@ import hashlib
 from database import db
 from utils import gen_token, gen_encrypt_key, gen_decrypt_key
 import datetime
-import timezone
+from time import timezone
 
 app = Flask(__name__)
 
@@ -20,7 +20,7 @@ Session(app)
 @app.errorhandler(werkzeug.exceptions.BadRequest)
 def handle_bad_request(e):
     print(e)
-    return Response(status=400)
+    return "", 400
 
 @app.route('/', methods = ['GET'])
 def hello():
@@ -32,52 +32,51 @@ def login():
     password = request.form['password']
     hashed_password = hmac.new(bytes(PASSWORD_HMAC_KEY, 'utf-8'), msg=bytes(password, 'utf-8')
                             , digestmod=hashlib.sha256).hexdigest()
-    user = db.query("SELECT id, username, role, permission FROM users WHERE username = %s AND password = %s", (username, hashed_password))
 
+    user = db.query("SELECT id, username, role, permission FROM users WHERE username = %s AND password = %s", (username, hashed_password))
+    print(user)
     if (len(user) != 1):
-        resp = Response(response="Wrong username or password", status=401)
-    else:
-        session["data"] = {"id": user[0]["id"], "username": user[0]["username"], "role": user[0]["role"], "permission": user[0]["permisison"]}
-        resp = Response(status=200)
-    return resp
+        return "Wrong username or password", 401
+    session["data"] = user[0]
+    return "", 200
     
 @app.route('/api/register', methods = ['POST'])
 def register():
-    if (not session["data"]):
-        return Response(status=401)
+
+
+    if (not request.cookies.get("session") or not session["data"]):
+        return "", 401
 
     if (session["data"]["role"] != "admin"):
-        return Response(status=403)
+        return "", 403
 
     username = request.form['username']
     password = request.form['password']
     role = request.form['role']
 
-    user = db.query("SELECT COUNT(1) FROM users WHERE username = %s", (username,))
+    user = db.query("SELECT COUNT(1) AS cnt FROM users WHERE username = %s", (username,))
 
     hashed_password = hmac.new(bytes(PASSWORD_HMAC_KEY, 'utf-8'), msg=bytes(password, 'utf-8')
                             , digestmod=hashlib.sha256).hexdigest()
 
-    if (user[0][0] != 0):
-        resp = Response(response="Username already exists", status=409)
-    else:
-        db.update("INSERT INTO users(username, password, role) VALUES (%s, %s, %s)", (username, hashed_password, role))
-        resp = Response(status=200)
-    return resp
+    if (user[0]['cnt'] != 0):
+        return 409, "Username already exists"
+    db.update("INSERT INTO users(username, password, role) VALUES (%s, %s, %s)", (username, hashed_password, role))
+    return "", 200
 
 @app.route('/api/parameters', methods = ['GET'])
 def parameters():
-    if (not session["data"]):
-        return Response(status=401)
+    if (not request.cookies.get("session") or not session["data"]):
+        return "", 401
     
     # TODO: gen param
     dict_param = {}
-    dict_param["token"] = gen_token({"permission": session["data"]["permission"], "username": session["data"]["username"], "exp": datetime.now(tz=timezone.utc) + 3600})
+    dict_param["token"] = gen_token({"permission": session["data"]["permission"], "username": session["data"]["username"], "exp": datetime.datetime.now(tz=datetime.timezone.utc).timestamp() + 3600})
     dict_param["decrypt_key"] = gen_decrypt_key()
     if (session["data"]["permission"]):
         dict_param["encrypt_key"] = gen_encrypt_key()
     
-    return Response(200, response=json.dumps(dict_param), mimetype="application/json")
+    return dict_param, 200, {'Content-Type': 'application/json'}
 
 if __name__ == '__main__':
     app.run(port=2808, debug=True)
--- authority_server/schema.sql
+++ authority_server/schema.sql
@@ -13,7 +13,6 @@ CREATE TABLE
     PRIMARY KEY (id)
   );
 
-INSERT INTO
-  users (username, password, role)
-VALUES
-  ("admin", "password", "admin") -- admin account
+INSERT INTO users (username, password, role, permission) VALUES ("admin", "22690f3254ccce4ff47abb8cf09785addc7a55e3b13c61b4b6a09bc5a3df2288", "admin", 1); -- admin account
+
+INSERT INTO users (username, password, role) VALUES ("patient", "22690f3254ccce4ff47abb8cf09785addc7a55e3b13c61b4b6a09bc5a3df2288", "patient"); 
