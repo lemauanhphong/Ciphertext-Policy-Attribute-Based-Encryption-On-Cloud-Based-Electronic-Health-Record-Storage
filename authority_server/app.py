da5606a689dc114f4a425cba82b2618deb08337c
woanmeo11 <woanmeo11@gmail.com>
2023-06-08 10:59:12 +0700

    handle ABE keys

--- authority_server/app.py
+++ authority_server/app.py
@@ -1,82 +1,101 @@
-from config import PASSWORD_HMAC_KEY, COOKIE_KEY
+import datetime
+import hashlib
+import hmac
+
+from config import COOKIE_KEY, PASSWORD_HMAC_KEY
+from database import db
 from flask import Flask, request, session
 from flask_session import Session
-import werkzeug
-import hmac
-import json
-import hashlib
-from database import db
-from utils import gen_token, gen_encrypt_key, gen_decrypt_key
-import datetime
-from time import timezone
+from utils import abe, gen_token
+from werkzeug.exceptions import BadRequest
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = COOKIE_KEY
+app.config["SECRET_KEY"] = COOKIE_KEY
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-@app.errorhandler(werkzeug.exceptions.BadRequest)
+
+@app.errorhandler(BadRequest)
 def handle_bad_request(e):
     print(e)
     return "", 400
 
-@app.route('/', methods = ['GET'])
+
+@app.route("/", methods=["GET"])
 def hello():
     return "Hello world"
 
-@app.route('/api/login', methods = ['POST'])
-def login():
-    username = request.form['username']
-    password = request.form['password']
-    hashed_password = hmac.new(bytes(PASSWORD_HMAC_KEY, 'utf-8'), msg=bytes(password, 'utf-8')
-                            , digestmod=hashlib.sha256).hexdigest()
 
-    user = db.query("SELECT id, username, role, permission FROM users WHERE username = %s AND password = %s", (username, hashed_password))
+@app.route("/api/login", methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    hashed_password = hmac.new(
+        bytes(PASSWORD_HMAC_KEY, "utf-8"), msg=bytes(password, "utf-8"), digestmod=hashlib.sha256
+    ).hexdigest()
+
+    user = db.query(
+        "SELECT id, username, role, permission FROM users WHERE username = %s AND password = %s",
+        (username, hashed_password),
+    )
     print(user)
-    if (len(user) != 1):
+    if len(user) != 1:
         return "Wrong username or password", 401
     session["data"] = user[0]
     return "", 200
-    
-@app.route('/api/register', methods = ['POST'])
+
+
+@app.route("/api/register", methods=["POST"])
 def register():
-
-
-    if (not request.cookies.get("session") or not session["data"]):
+    if not request.cookies.get("session") or not session["data"]:
         return "", 401
 
-    if (session["data"]["role"] != "admin"):
+    if session["data"]["role"] != "admin":
         return "", 403
 
-    username = request.form['username']
-    password = request.form['password']
-    role = request.form['role']
+    username = request.form["username"]
+    password = request.form["password"]
+    role = request.form["role"]
 
     user = db.query("SELECT COUNT(1) AS cnt FROM users WHERE username = %s", (username,))
 
-    hashed_password = hmac.new(bytes(PASSWORD_HMAC_KEY, 'utf-8'), msg=bytes(password, 'utf-8')
-                            , digestmod=hashlib.sha256).hexdigest()
+    hashed_password = hmac.new(
+        bytes(PASSWORD_HMAC_KEY, "utf-8"), msg=bytes(password, "utf-8"), digestmod=hashlib.sha256
+    ).hexdigest()
 
-    if (user[0]['cnt'] != 0):
+    if user[0]["cnt"] != 0:
         return 409, "Username already exists"
     db.update("INSERT INTO users(username, password, role) VALUES (%s, %s, %s)", (username, hashed_password, role))
     return "", 200
 
-@app.route('/api/parameters', methods = ['GET'])
-def parameters():
-    if (not request.cookies.get("session") or not session["data"]):
-        return "", 401
-    
-    # TODO: gen param
-    dict_param = {}
-    dict_param["token"] = gen_token({"permission": session["data"]["permission"], "username": session["data"]["username"], "exp": datetime.datetime.now(tz=datetime.timezone.utc).timestamp() + 3600})
-    dict_param["decrypt_key"] = gen_decrypt_key()
-    if (session["data"]["permission"]):
-        dict_param["encrypt_key"] = gen_encrypt_key()
-    
-    return dict_param, 200, {'Content-Type': 'application/json'}
 
-if __name__ == '__main__':
+@app.route("/api/parameters", methods=["GET"])
+def parameters():
+    if not request.cookies.get("session") or not session["data"]:
+        return "", 401
+
+    # TODO: handle generating secret key with proper attributes
+    dict_param = {}
+    dict_param["token"] = gen_token(
+        {
+            "permission": session["data"]["permission"],
+            "username": session["data"]["username"],
+            "exp": datetime.datetime.now(tz=datetime.timezone.utc).timestamp() + 3600,
+        }
+    )
+    dict_param["public_key"] = abe.get_public_key()
+
+    # TODO: handle attributes
+    # dict_param["secret_key"] = abe.gen_secret_key(attributes)
+
+    # TODO: handle write permission
+    # if session["data"]["permission"]:
+    #    dict_param["encrypt_key"] = gen_encrypt_key()
+
+    return dict_param, 200, {"Content-Type": "application/json"}
+
+
+if __name__ == "__main__":
     app.run(port=2808, debug=True)
--- authority_server/utils.py
+++ authority_server/utils.py
@@ -1,13 +1,34 @@
-from config import JWT_PRIVKEY
 import jwt
+from charm.adapters.abenc_adapt_hybrid import HybridABEnc
+from charm.core.engine.util import objectToBytes
+from charm.schemes.abenc.abenc_bsw07 import CPabe_BSW07
+from charm.toolbox.pairinggroup import PairingGroup
+from config import JWT_PRIVKEY
+
+
+class ABE:
+    def __init__(self):
+        self.pairing_group = PairingGroup("SS512")
+        self.hyb_abe = HybridABEnc(CPabe_BSW07(self.pairing_group), self.pairing_group)
+        self.public_key, self.master_key = self.hyb_abe.setup()
+
+        with open(".keys", "wb") as f:
+            f.write(self.get_public_key())
+            f.write(b"\n")
+            f.write(self.get_master_key())
+
+    def get_public_key(self):
+        return objectToBytes(self.public_key, self.pairing_group)
+
+    def get_master_key(self):
+        return objectToBytes(self.master_key, self.pairing_group)
+
+    def gen_secret_key(self, attributes: list):
+        return objectToBytes(self.hyb_abe.keygen(self.public_key, self.master_key, attributes), self.pairing_group)
+
 
 def gen_token(data):
     return jwt.encode(data, JWT_PRIVKEY, algorithm="EdDSA")
 
-# TODO: implement this
-def gen_decrypt_key():
-    return ""
 
-# TODO: implement this
-def gen_encrypt_key():
-    return ""
+abe = ABE()
