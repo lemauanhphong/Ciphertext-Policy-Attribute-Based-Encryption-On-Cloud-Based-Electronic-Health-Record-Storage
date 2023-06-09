4082287320cf80de426de1f7e69e14ea93cc332a
woanmeo11 <woanmeo11@gmail.com>
2023-06-09 09:21:15 +0700

    add columns

--- cloud/schema.sql
+++ cloud/schema.sql
@@ -7,9 +7,9 @@ CREATE TABLE
     id INT NOT NULL AUTO_INCREMENT,
     uid INT NOT NULL,
     creator INT NOT NULL,
-    date DATE DEFAULT CURRENT_DATE,
-    file_location VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
+    date DATE DEFAULT CURRENT_DATE,
+    data LONGBLOB,
     PRIMARY KEY (id)
   );
 
@@ -18,11 +18,11 @@ CREATE TABLE
     id INT NOT NULL AUTO_INCREMENT,
     uid INT NOT NULL,
     creator INT NOT NULL,
-    date DATE DEFAULT CURRENT_DATE,
-    file_location VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
+    date DATE DEFAULT CURRENT_DATE,
     date_of_birth DATE,
     address VARCHAR(255) NOT NULL,
+    data LONGBLOB,
     PRIMARY KEY (id)
   );
 
@@ -32,8 +32,8 @@ CREATE TABLE
     uid INT NOT NULL,
     creator INT NOT NULL,
     name VARCHAR(255) NOT NULL,
-    file_location VARCHAR(255) NOT NULL,
     date DATE DEFAULT CURRENT_DATE,
+    data LONGBLOB,
     PRIMARY KEY (id)
   );
 
@@ -43,7 +43,7 @@ CREATE TABLE
     uid INT NOT NULL,
     creator INT NOT NULL,
     name VARCHAR(255) NOT NULL,
-    file_location VARCHAR(255) NOT NULL,
     date DATE DEFAULT CURRENT_DATE,
+    data LONGBLOB,
     PRIMARY KEY (id)
   );
