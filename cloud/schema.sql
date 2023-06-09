e9cfe239298a98e30103b8c9eed80178df8ecef4
woanmeo11 <woanmeo11@gmail.com>
2023-06-09 11:42:00 +0700

    remodel

--- cloud/schema.sql
+++ cloud/schema.sql
@@ -6,9 +6,10 @@ CREATE TABLE
   health_records (
     id INT NOT NULL AUTO_INCREMENT,
     uid INT NOT NULL,
-    creator INT NOT NULL,
+    uploader_id INT NOT NULL,
     name VARCHAR(255) NOT NULL,
     date DATE DEFAULT CURRENT_DATE,
+    description VARCHAR(255) NOT NULL,
     data LONGBLOB,
     PRIMARY KEY (id)
   );
@@ -17,22 +18,23 @@ CREATE TABLE
   person_profiles (
     id INT NOT NULL AUTO_INCREMENT,
     uid INT NOT NULL,
-    creator INT NOT NULL,
+    uploader_id INT NOT NULL,
     name VARCHAR(255) NOT NULL,
     date DATE DEFAULT CURRENT_DATE,
     date_of_birth DATE,
     address VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
     data LONGBLOB,
     PRIMARY KEY (id)
   );
 
 CREATE TABLE
-  research (
+  researches (
     id INT NOT NULL AUTO_INCREMENT,
-    uid INT NOT NULL,
-    creator INT NOT NULL,
+    uploader_id INT NOT NULL,
     name VARCHAR(255) NOT NULL,
     date DATE DEFAULT CURRENT_DATE,
+    description VARCHAR(255) NOT NULL,
     data LONGBLOB,
     PRIMARY KEY (id)
   );
@@ -41,9 +43,9 @@ CREATE TABLE
   financials (
     id INT NOT NULL AUTO_INCREMENT,
     uid INT NOT NULL,
-    creator INT NOT NULL,
     name VARCHAR(255) NOT NULL,
     date DATE DEFAULT CURRENT_DATE,
+    description VARCHAR(255) NOT NULL,
     data LONGBLOB,
     PRIMARY KEY (id)
   );
