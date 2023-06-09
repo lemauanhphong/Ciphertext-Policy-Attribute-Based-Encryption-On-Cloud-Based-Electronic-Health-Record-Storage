bd05c6dd0c52043347eafe2e5c2cf34414d25817
woanmeo11 <woanmeo11@gmail.com>
2023-06-09 17:27:26 +0700

    add multiple attributes to the secret key

--- authority_server/app.py
+++ authority_server/app.py
@@ -31,8 +31,8 @@ def hello():
 
 @app.route("/api/login", methods=["POST"])
 def login():
-    username = request.form["username"]
-    password = request.form["password"]
+    username = request.json["username"]
+    password = request.json["password"]
 
     hashed_password = hmac.new(
         bytes(PASSWORD_HMAC_KEY, "utf-8"), msg=bytes(password, "utf-8"), digestmod=hashlib.sha256
@@ -60,9 +60,12 @@ def register():
     if "admin" not in session["data"]["attributes"]["roles"]:
         return "", 403
 
-    username = request.form["username"]
-    password = request.form["password"]
-    roles = request.form["roles"].replace(" ", "").split(",")
+    username = request.json["username"]
+    password = request.json["password"]
+
+    attributes = {}
+    for attr, vals in request.json["attributes"].items():
+        attributes[attr.upper()] = [val.upper() for val in vals]
 
     user = db.query("SELECT COUNT(1) AS cnt FROM users WHERE username = %s", (username,))
 
@@ -75,7 +78,7 @@ def register():
 
     db.update(
         "INSERT INTO users(username, password, attributes) VALUES (%s, %s, %s)",
-        (username, hashed_password, json.dumps({"roles": roles})),
+        (username, hashed_password, json.dumps(attributes)),
     )
     return "", 200
 
@@ -84,6 +87,11 @@ def register():
 def parameters():
     if not request.cookies.get("session") or not session["data"]:
         return "", 401
+
+    abe_attributes = []
+    for attr, vals in session["data"]["attributes"].items():
+        for val in vals:
+            abe_attributes.append(attr + "@" + val)
 
     dict_param = {
         "token": gen_token(
@@ -94,7 +102,7 @@ def parameters():
             }
         ),
         "public_key": abe.get_public_key().decode(),
-        "secret_key": abe.gen_secret_key(session["data"]["attributes"]["roles"]).decode(),
+        "secret_key": abe.gen_secret_key(abe_attributes).decode(),
     }
 
     return dict_param, 200, {"Content-Type": "application/json"}
