f02f793ebb479ee06e9a072260664987fe1a92ea
woanmeo11 <woanmeo11@gmail.com>
2023-06-17 02:20:31 +0700

    using docker secrets and fix nginx limit_req 503

--- authority_server/web/utils.py
+++ authority_server/web/utils.py
@@ -1,8 +1,9 @@
 import jwt
 from charm.adapters.abenc_adapt_hybrid import HybridABEnc
-from charm.core.engine.util import objectToBytes
+from charm.core.engine.util import bytesToObject, objectToBytes
 from charm.schemes.abenc.abenc_bsw07 import CPabe_BSW07
 from charm.toolbox.pairinggroup import PairingGroup
+
 from config import JWT_PRIVKEY
 
 
@@ -17,12 +18,12 @@ class ABE:
     def __init__(self):
         self.pairing_group = PairingGroup("SS512")
         self.hyb_abe = HybridABEnc(CPabe_BSW07(self.pairing_group), self.pairing_group)
-        self.public_key, self.master_key = self.hyb_abe.setup()
 
-        with open(".keys", "wb") as f:
-            f.write(self.get_public_key())
-            f.write(b"\n")
-            f.write(self.get_master_key())
+        with open("/run/secrets/abe_keys", "rb") as f:
+            self.public_key, self.master_key = f.read().splitlines()
+
+        self.public_key = bytesToObject(self.public_key, self.pairing_group)
+        self.master_key = bytesToObject(self.master_key, self.pairing_group)
 
     def get_public_key(self):
         return objectToBytes(self.public_key, self.pairing_group)
