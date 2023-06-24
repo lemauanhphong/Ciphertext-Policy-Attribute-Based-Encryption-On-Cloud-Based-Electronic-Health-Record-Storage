356004fae02c08ea088dea98fbf37a5c472bf949
woanmeo11 <81372907+woanmeo11@users.noreply.github.com>
2023-06-24 17:40:43 +0700

    add autocommit

--- authority_server/web/database.py
+++ authority_server/web/database.py
@@ -21,6 +21,7 @@ class Database:
                 port=3306,
                 database="authority_server",
             )
+            self.conn.autocommit = True
             self.cursor = self.conn.cursor(dictionary=True)
         except mariadb.Error:
             print_exc()
@@ -43,7 +44,6 @@ class Database:
                 self.cursor.execute(stmt, param)
             else:
                 self.cursor.execute(stmt)
-            self.conn.commit()
             return True
         except Exception:
             self.conn.rollback()
